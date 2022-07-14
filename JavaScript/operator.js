31968ca527ed5c5dd3e4bb658e083fe83d888bf5
Melonisjy <dhy2457@naver.com>
2022-07-14 14:40:12 +0900

    operator.js

---
+++ JavaScript/operator.js
@@ -0,0 +1,18 @@
+// 1. String concatenation
+console.log('my' + 'cat');
+console.log('1' + 2);
+console.log(`string literals: 1 + 2 = ${1 + 2}`);
+
+// 2. Numeric operators
+console.log(1 + 1); // add
+console.log(1 - 1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); // exponentiation
+
+// 3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter  + 1;
+// preIncrement = counter;
