fe40287f41ac55ba08e64f2530f3c548e4ceb1b7
Melonisjy <dhy2457@naver.com>
2022-07-15 16:42:34 +0900

    function.js

---
+++ JavaScript/function.js
@@ -0,0 +1,141 @@
+// Function
+// - fundamental building block in the program
+// - subprogram can be used multiple times
+// - performs a task or calculates a value
+
+// 1. Function declaration
+// function name(param1, param2) { body... return; }
+// one function === one thing
+// naming: doSomething, command, verb
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS
+
+function printHello() {
+    console.log('Hello');
+}
+printHello();
+
+function log(message) {
+    console.log(message);
+}
+log('Hello@');
+log(1234);
+
+// 2. Parameters
+// primitive parameters: passed by value
+// object parameters: passed by reference
+function changeName(obj) {
+    obj.name = 'coder';
+}
+const melon = { name: 'melon'};
+changeName(melon);
+console.log(melon);
+
+// 3. Default parameters (added in ES6)
+function showMessage(message, from = 'unknown') {
+    console.log(`${message} by ${from}`);
+}
+showMessage('Hi!');
+
+// 4. Rest parameters (added in ES6)
+function printAll(...args) {
+    for (let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+    }
+
+    for (const arg of args) {
+        console.log(arg);
+    }
+
+    args.forEach((arg) => console.log(arg));
+}
+printAll('dream', 'coding', 'melon');
+
+// 5. Local scope
+let globalMessage = 'global'; // global variable
+function printMessage() {
+    let message = 'hello';
+    console.log(message); // local variable
+    console.log(globalMessage);
+    function printAnother() {
+        console.log(message);
+        let childMessage = 'hello';
+    }
+}
+printMessage();
+
+// 6. Return a value
+function sum(a, b) {
+    return a + b;
+}
+const result = sum(1, 2); // 3
+console.log(`sum: ${sum(1, 2)}`);
+
+// 7. Early return, early exit
+// bad
+function upgradeUser(user) {
+    if (user.point > 10) {
+        // long upgrade logic...
+    }
+}
+
+// good
+function upgradeUser(user) {
+    if (user.point > 10) {
+        return;
+    }
+    // long upgrade logic...
+}
+
+// 1. Function expression
+// a function declaration can be called earlier than it is defined. (hoisted)
+// a function expression is created when the execution reaches it.
+const print = function () { // anonymous function (함수에 이름이 없는 것.)
+    console.log('print');
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+
+// 2. Callback function using function expression
+function randomQuiz(answer, printYes, printNo) {
+    if (answer === 'love you') {
+        printYes();
+    } else {
+        printNo();
+    }
+}
+
+// anonymous function
+const printYes = function() {
+    console.log('yes!');
+};
+
+// name function
+// better debugging in debugger's stack traces
+// recursions
+const printNo = function print() {
+    console.log('no!');
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+
+// Arrow function
+// always anonymous
+// const simplePrint = function () {
+//     console.log('simplePrint!');
+// };
+
+const simplePrint = () => console.log('simplePrint');
+const add = (a, b) => a + b;
+const simpleMultiply = (a, b) => {
+    // do something more
+    return a * b;
+};
+
+// IIFE: Immediately Invoked Function Expression
+(function hello() {
+    console.log('IIFE');
+})();
