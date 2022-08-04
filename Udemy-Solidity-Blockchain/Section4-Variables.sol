383e8cb919a2b9d57e472c33b19b3bcf223b6fc3
Melonisjy <dhy2457@naver.com>
2022-08-05 00:46:21 +0900

    first commit

---
+++ Udemy-Solidity-Blockchain/Section4-Variables.sol
@@ -0,0 +1,46 @@
+### Variables.sol
+
+```solidity
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.15;
+
+contract WorkingWithVariables {
+    uint256 public myUint;
+
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+
+    bool public myBool;
+
+    function setMyBool(bool _myBool) public {
+        myBool = _myBool;
+    }
+
+    uint8 public myUint8;
+
+    function incrementUint() public {
+        myUint8++;
+    }
+
+    function decrementUint() public {
+        myUint8--;
+    }
+
+    address public myAddress;
+
+    function setAddress(address _address) public {
+        myAddress = _address;
+    }
+
+    function getBalanceOfAddress() public view returns(uint) {
+        return myAddress.balance; 
+    }
+
+    string public myString;
+
+    function setMyString(string memory _myString) public {
+        myString = _myString;
+    }
+}
+```
