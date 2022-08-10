3f78e65d1f5d56d1b40664e75abead0601002d1e
Melonisjy <dhy2457@naver.com>
2022-08-11 00:58:43 +0900

    first commit

---
+++ Udemy-Solidity-Blockchain/ExceptionExample.sol
@@ -0,0 +1,19 @@
+// SPDX-Licence-Identifier: UNLICENCE
+pragma solidity ^0.5.13;
+
+contract ExceptionExample {
+
+    mapping(address => uint64) public balanceReceived;
+
+    function receiveMoney() public payable {
+        assert(balanceReceived[msg.sender] + uint64(msg.value) >= balanceReceived[msg.sender]);
+        balanceReceived[msg.sender] += uint64(msg.value);
+    }
+
+    function withdrawMoney(address payable _to, uint64 _amount) public {
+        require(_amount <= balanceReceived[msg.sender], "You don't have enough ether!");
+        assert(balanceReceived[msg.sender] >= balanceReceived[msg.sender] - _amount);
+        balanceReceived[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
+}
